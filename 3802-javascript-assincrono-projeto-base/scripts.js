2cb04489333efead34bf7c273c34a29e3fadccf8
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-30 10:44:29 -0300

    Adicionando o elemento para remover tags.

--- 3802-javascript-assincrono-projeto-base/scripts.js
+++ 3802-javascript-assincrono-projeto-base/scripts.js
@@ -52,3 +52,10 @@ inputTags.addEventListener("keypress", (evento) => {
         }
     }
 })
+
+listaTags.addEventListener("click", (evento) => {
+    if (evento.target.classList.contains("remove-tag")) {
+    const tagQueQueremosRemover = evento.target.parentElement
+    listaTags.removeChild(tagQueQueremosRemover);
+    }
+})
