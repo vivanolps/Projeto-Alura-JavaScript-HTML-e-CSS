a583fadd4f8f3a8ce4bcb18fc514decc3edf7b58
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-30 10:02:54 -0300

    Inserindo tags

--- 3802-javascript-assincrono-projeto-base/scripts.js
+++ 3802-javascript-assincrono-projeto-base/scripts.js
@@ -36,3 +36,19 @@ inputUpload.addEventListener("change", async (evento) => {
         }
     }
 })
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+inputTags.addEventListener("keypress", (evento) => {
+    if (evento.key === "Enter") {
+        evento.preventDefault();
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== "") {
+            const tagNova = document.createElement("li");
+            tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+            listaTags.appendChild(tagNova);
+            inputTags.value = "";
+        }
+    }
+})
