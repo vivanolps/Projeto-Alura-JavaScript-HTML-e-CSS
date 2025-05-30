1c5ae72c1aa733690fc68dbf2f04d412f419e43d
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-30 09:46:06 -0300

    Funções assíncronas

--- 3802-javascript-assincrono-projeto-base/scripts.js
+++ 3802-javascript-assincrono-projeto-base/scripts.js
@@ -19,3 +19,20 @@ function lerConteudoDoArquivo(arquivo) {
         leitor.readAsDataURL(arquivo)
     })
 }
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (erro) {
+            console.error("Erro na leitura do arquivo.")
+        }
+    }
+})
