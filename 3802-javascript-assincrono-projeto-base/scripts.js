92d77a777ffd83fd25a8f333d8f00c6eaac3fc12
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-30 09:33:26 -0300

    Colocando promessas.

--- 3802-javascript-assincrono-projeto-base/scripts.js
+++ 3802-javascript-assincrono-projeto-base/scripts.js
@@ -4,3 +4,18 @@ const inputUpload = document.getElementById("image-upload");
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name })
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+
+        leitor.readAsDataURL(arquivo)
+    })
+}
